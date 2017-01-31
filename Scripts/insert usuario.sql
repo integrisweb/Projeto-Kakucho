5a6e1f71c62c2718923f1073d846115a8af58bf3
Washington Vale <ottovale@gmail.com>
2017-01-31 18:00:14 -0300

    Add files via upload

--- Scripts/insert usuario.sql
+++ Scripts/insert usuario.sql
@@ -1,1 +1,8 @@
-insert into usuario(nome_usuario,senha_usuario,situacao_usuario) values('admin','zcx#21','N');
+insert into usuario(nome_usuario,senha_usuario,situacao_usuario) 
+values('admin','zcx#21','N');
+
+insert into usuario(nome_usuario,senha_usuario,situacao_usuario) 
+values('gyouenshi','123456','N');
+
+insert into usuario( nome_usuario, senha_usuario, situacao_usuario ) 
+values('wvale','vale2000','N')
