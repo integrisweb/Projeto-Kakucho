420418c1aca8f95ec3fc105ac38cfb1bbb42256e
Washington Vale <ottovale@gmail.com>
2017-06-20 14:17:14 -0300

    Add files via upload

--- Tabelas/tipo_culto.sql
+++ Tabelas/tipo_culto.sql
@@ -3,6 +3,6 @@ CREATE TABLE `tipo_culto` (
 `id_culto` INT NOT NULL COMMENT'Código do Culto' , 
 `nome_culto` VARCHAR(30) NOT NULL COMMENT 'Tipo do Culto' ,
 `especial` CHAR(1) NOT NULL COMMENT 'Culto Especial (S/N)' , 
-`situacao_culto`CHAR(1) NOT NULL COMMENT 'Situação do Culto' , 
-PRIMARY KEY (`id_culto`) COMMENT 'id_culto' 
+`situacao_culto` CHAR(1) NOT NULL COMMENT 'Situação do Culto' , 
+PRIMARY KEY (`id_tipo_culto`) COMMENT 'id_tipo_culto' 
 );
