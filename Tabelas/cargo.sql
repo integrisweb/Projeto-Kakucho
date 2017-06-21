c7fe67893aff190b4c3eaf5647607f271519bc26
Washington Vale <ottovale@gmail.com>
2017-06-21 13:03:43 -0300

    Add files via upload

--- Tabelas/cargo.sql
+++ Tabelas/cargo.sql
@@ -1,6 +1,7 @@
+
 CREATE TABLE `cargo` (
- `id_cargo` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Campo CÛdigo do Cargo, Auto Incremento',
+ `id_cargo` int NOT NULL AUTO_INCREMENT COMMENT 'Campo C√≥digo do Cargo, Auto Incremento',
  `nome_cargo` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Nome do Cargo para Membros da HBS',
- `situacao_cargo` char(1) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'SituaÁ„o do Cargo para HBS',
- PRIMARY KEY (`id_cargo`) COMMENT 'Chave Prim·ria'
+ `situacao_cargo` char(1) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Situa√ß√£o do Cargo para HBS',
+ PRIMARY KEY (`id_cargo`) COMMENT 'Chave Prim√°ria'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Tabela de Cargos para HBS'
