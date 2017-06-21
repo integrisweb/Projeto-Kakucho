70696e1db460d5717c8591667371ef3e96e82633
Washington Vale <ottovale@gmail.com>
2017-06-21 17:10:46 -0300

    Add files via upload

--- Tabelas/doacao.sql
+++ Tabelas/doacao.sql
@@ -1,8 +1,11 @@
 CREATE TABLE `doacao` ( 
 `id_doacao` INT NOT NULL AUTO_INCREMENT COMMENT 'Código da Doação',
-`id_templo` INT NOT NULL COMMENT 'Código do Templo',
-`id_tipo_doacao` INT NOT NULL 'Código da Doação',
-`data_doacao` DATETIME NULL COMMENT 'Data de Doacao',
-`situacao_doacao`CHAR(1) NOT NULL COMMENT 'Situação da Doação (A)tivo (I)nativo' , 
-PRIMARY KEY (`id_culto`) COMMENT 'id_culto' 
+`id_templo_doacao` INT NOT NULL COMMENT 'Código do Templo Doação',
+`id_templo_fiel` INT NOT NULL COMMENT 'Código do Templo do Fiel',
+`id_tipo_doacao` INT NOT NULL COMMENT 'Código da Doação',
+`id_fiel` INT NOT NULL COMMENT 'Código do Fiel',
+`valor_doacao_fiel` DECIMAL(15,2) NOT NULL COMMENT 'valor recebido Templo Doação',
+`data_doacao` DATETIME NOT NULL COMMENT 'Data de Doacao',
+`situacao_doacao` CHAR(1) NOT NULL COMMENT 'Situação da Doação (A)tivo (I)nativo' , 
+PRIMARY KEY (`id_doacao`) COMMENT 'id_doacao' 
 );
